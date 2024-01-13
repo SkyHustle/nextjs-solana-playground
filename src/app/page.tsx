db959517b4808da2390d0281b6942e30537ede6c
Dmitry Sky <dmitry.sky@icloud.com>
2024-01-13 13:08:33 -0700

    Connect or disconnect phantom wallet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
 "use client";
 import { NextPage } from "next";
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
 import { clusterApiUrl } from "@solana/web3.js";
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
     const endpoint = clusterApiUrl("devnet");
     const wallet = new PhantomWalletAdapter();
 
     return (
         <ConnectionProvider endpoint={endpoint}>
             <WalletProvider wallets={[wallet]}>
-                {wallet.connected ? <div>Wallet Connected</div> : <div> Wallet Not connected</div>}
+                <WalletModalProvider>
+                    <WalletMultiButton />
+                    <p>Put the rest of your app here</p>
+                </WalletModalProvider>
             </WalletProvider>
         </ConnectionProvider>
     );
