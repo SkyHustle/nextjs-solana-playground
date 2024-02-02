b0019e266b078c312c987fcd24f42ff03ecd4653
Dmitry Sky <dmitry.sky@icloud.com>
2024-02-02 13:37:55 -0700

    Chainlink expects the provider to be an AnchorProvider, even though it will work with a solana wallet adapter provider

--- src/components/chainlink/sol-usd-price-feed.tsx
+++ src/components/chainlink/sol-usd-price-feed.tsx
@@ -1,35 +1,41 @@
 import React, { useState, useEffect } from "react";
-import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { useConnection, useWallet, useAnchorWallet } from "@solana/wallet-adapter-react";
 import { OCR2Feed } from "@chainlink/solana-sdk";
-import { PublicKey } from "@solana/web3.js";
+import { PublicKey, Transaction } from "@solana/web3.js";
+import { AnchorProvider } from "@project-serum/anchor";
 
 const ChainlinkPriceFeed = () => {
   const [price, setPrice] = useState<number>();
   const { connection } = useConnection();
-  const { publicKey } = useWallet();
+  const wallet = useAnchorWallet();
 
   useEffect(() => {
-    if (!publicKey) {
+    if (!wallet) {
       console.log("Wallet not connected");
       return;
+    } else {
+      console.log("Wallet connected");
     }
 
     const CHAINLINK_FEED_ADDRESS = "99B2bTijsU6f1GCT73HmdR7HCFFjGMBcPZY6jZ96ynrR";
     const CHAINLINK_PROGRAM_ID = new PublicKey("cjg3oHmg9uuPsP8D6g29NWvhySJkdYdAo9D25PRbKXJ");
     const feedAddress = new PublicKey(CHAINLINK_FEED_ADDRESS);
 
-    let dataFeed;
-    let listener;
+    let dataFeed: OCR2Feed | undefined;
+    let listener: number | undefined;
 
     const subscribeToPriceFeed = async () => {
       try {
-        dataFeed = await OCR2Feed.load(CHAINLINK_PROGRAM_ID, { connection });
+        // Creating an AnchorProvider
+        const provider = new AnchorProvider(connection, wallet, {});
+
+        dataFeed = await OCR2Feed.load(CHAINLINK_PROGRAM_ID, provider);
 
         listener = dataFeed.onRound(feedAddress, (event) => {
           const rawPrice = event.answer.toNumber();
-          const decimals = 8; // This should be set based on the actual decimals used by the price feed
+          const decimals = 8;
           const priceInDollars = rawPrice / Math.pow(10, decimals);
-          const formattedPrice = parseFloat(priceInDollars.toFixed(2)); // Limit to 2 decimal places and convert back to number
+          const formattedPrice = parseFloat(priceInDollars.toFixed(2));
           setPrice(formattedPrice);
         });
       } catch (error) {
@@ -44,9 +50,9 @@ const ChainlinkPriceFeed = () => {
         dataFeed.removeListener(listener);
       }
     };
-  }, [publicKey, connection]);
+  }, [wallet, connection]);
 
-  return <div>{price ? `SOL Price: $${price}` : "Loading..."}</div>;
+  return <div>{price ? `Live SOL Price: $${price}` : "Loading..."}</div>;
 };
 
 export default ChainlinkPriceFeed;
