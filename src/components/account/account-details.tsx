7488e9f98cb0602f951d7b60510f5d630b4c7106
Dmitry Sky <dmitry.sky@icloud.com>
2024-02-03 14:06:33 -0700

    Use tailwind forms plug-in for nicer looking inputs

--- src/components/account/account-details.tsx
+++ src/components/account/account-details.tsx
@@ -38,7 +38,14 @@ export default function AccountDetails({ address }: { address: PublicKey }) {
           >
             <PaperAirplaneIcon className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
             Airdrop
-            <TransactionModal hide={() => setShowAirdropModal(false)} address={address} show={showAirdropModal} />
+            <TransactionModal
+              hide={() => setShowAirdropModal(false)}
+              address={address}
+              show={showAirdropModal}
+              title="Airdrop"
+            >
+              <PaperAirplaneIcon className="-ml-0.5 mr-1.5 h-7 w-7 text-gray-400" aria-hidden="true" />
+            </TransactionModal>
           </button>
         </span>
 
@@ -97,14 +104,25 @@ export function AccountBalance({ address }: { address: PublicKey }) {
 
 import { Fragment, useRef, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
-import { CheckIcon } from "@heroicons/react/24/outline";
 
-export function TransactionModal({ hide, show, address }: { hide: () => void; show: boolean; address: PublicKey }) {
+export function TransactionModal({
+  hide,
+  show,
+  address,
+  title,
+  children,
+}: {
+  hide: () => void;
+  show: boolean;
+  address: PublicKey;
+  title: string;
+  children: React.ReactNode;
+}) {
   const cancelButtonRef = useRef(null);
 
   return (
     <Transition.Root show={show} as={Fragment}>
-      <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={hide}>
+      <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={() => hide}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -130,29 +148,28 @@ export function TransactionModal({ hide, show, address }: { hide: () => void; sh
             >
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
                 <div>
-                  <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
-                    <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
-                  </div>
+                  <div className="mx-auto flex items-center justify-center">{children}</div>
                   <div className="mt-3 text-center sm:mt-5">
-                    <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
-                      Payment successful
+                    <Dialog.Title as="h2" className="text-base font-semibold leading-6 text-gray-900">
+                      {title}
                     </Dialog.Title>
                     <div className="mt-2">
-                      <p className="text-sm text-gray-500">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius aliquam laudantium explicabo
-                        pariatur iste dolorem animi vitae error totam. At sapiente aliquam accusamus facere veritatis.
-                      </p>
+                      <div>
+                        <label htmlFor="amount" className="sr-only">
+                          Quantity
+                        </label>
+                        <input
+                          type="number"
+                          name="amount"
+                          id="amount"
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                          placeholder="How many SOL to airdrop?"
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>
                 <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
-                  <button
-                    type="button"
-                    className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 sm:col-start-2"
-                    onClick={hide}
-                  >
-                    Close
-                  </button>
                   <button
                     type="button"
                     className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
@@ -160,6 +177,14 @@ export function TransactionModal({ hide, show, address }: { hide: () => void; sh
                     ref={cancelButtonRef}
                   >
                     Request Airdrop
+                  </button>
+
+                  <button
+                    type="button"
+                    className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 sm:col-start-2"
+                    onClick={hide}
+                  >
+                    Close
                   </button>
                 </div>
               </Dialog.Panel>
