cbbca8edae4daa09389e9df222ad400d77d1a6d0
Dmitry Sky <dmitry.sky@icloud.com>
2024-02-03 14:34:18 -0700

    Make modal more dumb and more composable

--- src/components/account/account-details.tsx
+++ src/components/account/account-details.tsx
@@ -12,7 +12,9 @@ import ChainlinkPriceFeed from "../chainlink/sol-usd-price-feed";
 import { ExplorerLink, Ellipsify } from "../ui/link-display";
 
 export default function AccountDetails({ address }: { address: PublicKey }) {
-  const [showAirdropModal, setShowAirdropModal] = useState(false);
+  const [showTransactionModal, setShowTransactionModal] = useState(false);
+  const [airdropAmount, setAirdropAmount] = useState("");
+  console.log(`Airdrop Amount: ${airdropAmount} SOL`);
 
   return (
     <div className="lg:flex lg:items-center lg:justify-between pt-8 pb-8">
@@ -34,17 +36,55 @@ export default function AccountDetails({ address }: { address: PublicKey }) {
           <button
             type="button"
             className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-            onClick={() => setShowAirdropModal(true)}
+            onClick={() => setShowTransactionModal(true)}
           >
             <PaperAirplaneIcon className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
             Airdrop
-            <TransactionModal
-              hide={() => setShowAirdropModal(false)}
-              address={address}
-              show={showAirdropModal}
-              title="Airdrop"
-            >
-              <PaperAirplaneIcon className="-ml-0.5 mr-1.5 h-7 w-7 text-gray-400" aria-hidden="true" />
+            <TransactionModal hide={() => setShowTransactionModal(false)} show={showTransactionModal}>
+              <div>
+                <div>
+                  <div className="mx-auto flex items-center justify-center">
+                    <PaperAirplaneIcon className="-ml-0.5 mr-1.5 h-7 w-7 text-gray-400" aria-hidden="true" />
+                  </div>
+                  <div className="mt-3 text-center sm:mt-5">
+                    <h2 className="text-base font-semibold leading-6 text-gray-900">Airdrop</h2>
+                    <div className="mt-2">
+                      <div>
+                        <label htmlFor="amount" className="sr-only">
+                          Quantity
+                        </label>
+                        <input
+                          type="number"
+                          step="any"
+                          name="amount"
+                          id="amount"
+                          min="1"
+                          value={airdropAmount}
+                          onChange={(e) => setAirdropAmount(e.target.value)}
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                          placeholder="How many SOL to airdrop?"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+                  <button
+                    type="button"
+                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
+                    onClick={() => setShowTransactionModal(false)}
+                  >
+                    Request Airdrop
+                  </button>
+                  <button
+                    type="button"
+                    className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 sm:col-start-2"
+                    onClick={() => setShowTransactionModal(false)}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
             </TransactionModal>
           </button>
         </span>
@@ -102,27 +142,21 @@ export function AccountBalance({ address }: { address: PublicKey }) {
   );
 }
 
-import { Fragment, useRef, useState } from "react";
+import { Fragment, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 
 export function TransactionModal({
   hide,
   show,
-  address,
-  title,
   children,
 }: {
   hide: () => void;
   show: boolean;
-  address: PublicKey;
-  title: string;
   children: React.ReactNode;
 }) {
-  const cancelButtonRef = useRef(null);
-
   return (
     <Transition.Root show={show} as={Fragment}>
-      <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={() => hide}>
+      <Dialog as="div" className="relative z-10" onClose={() => hide}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -147,46 +181,7 @@ export function TransactionModal({
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-                <div>
-                  <div className="mx-auto flex items-center justify-center">{children}</div>
-                  <div className="mt-3 text-center sm:mt-5">
-                    <Dialog.Title as="h2" className="text-base font-semibold leading-6 text-gray-900">
-                      {title}
-                    </Dialog.Title>
-                    <div className="mt-2">
-                      <div>
-                        <label htmlFor="amount" className="sr-only">
-                          Quantity
-                        </label>
-                        <input
-                          type="number"
-                          name="amount"
-                          id="amount"
-                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                          placeholder="How many SOL to airdrop?"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
-                  <button
-                    type="button"
-                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
-                    onClick={hide}
-                    ref={cancelButtonRef}
-                  >
-                    Request Airdrop
-                  </button>
-
-                  <button
-                    type="button"
-                    className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 sm:col-start-2"
-                    onClick={hide}
-                  >
-                    Close
-                  </button>
-                </div>
+                {children}
               </Dialog.Panel>
             </Transition.Child>
           </div>
