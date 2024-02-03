35eba24f070b7ee6bc9e33b37d055dd221e6ce0d
Dmitry Sky <dmitry.sky@icloud.com>
2024-02-03 15:05:27 -0700

    Extract Transaction modal into it's own component

--- src/components/account/account-details.tsx
+++ src/components/account/account-details.tsx
@@ -11,6 +11,8 @@ import { useConnection } from "@solana/wallet-adapter-react";
 import ChainlinkPriceFeed from "../chainlink/sol-usd-price-feed";
 import { ExplorerLink, Ellipsify } from "../ui/link-display";
 import { useQueryClient, useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { TransactionModal } from "../ui/transaction-modal";
 
 export default function AccountDetails({ address }: { address: PublicKey }) {
   const [showTransactionModal, setShowTransactionModal] = useState(false);
@@ -148,55 +150,6 @@ export function AccountBalance({ address }: { address: PublicKey }) {
         {query.data ? <span>{Math.round((query.data / LAMPORTS_PER_SOL) * 100000) / 100000}</span> : "..."} SOL
       </h2>
     </div>
-  );
-}
-
-import { Fragment, useState } from "react";
-import { Dialog, Transition } from "@headlessui/react";
-
-export function TransactionModal({
-  hide,
-  show,
-  children,
-}: {
-  hide: () => void;
-  show: boolean;
-  children: React.ReactNode;
-}) {
-  return (
-    <Transition.Root show={show} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={() => hide}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-        </Transition.Child>
-
-        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-                {children}
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
-        </div>
-      </Dialog>
-    </Transition.Root>
   );
 }
 
